8b69b4633482d5efe342d36599d8eec1af6b7e30
Dev Zack <zachariebigboss@gmail.com>
2025-05-07 16:28:46 +0100

    Eslint mofication

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": [
+        "error",
+        {
+          forbid: [], // Liste vide = autorise ' et " sans restriction
+        },
+      ],
+    }
+  }
 ];
 
 export default eslintConfig;
